e0bd808fbf6cbbc81b8ccd7b31ed750d9fc017da
ReconChief <32560933+ReconChief@users.noreply.github.com>
2019-03-31 11:13:06 -0400

    Tutorial Trigger Done

---
+++ Assets/Scripts/TutorialWorld/MissileTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileTrigger : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject missileTutorial;
+
+    private PlayerController pc;
+    private MissileTrigger missileTrigger;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pc = player.GetComponent<PlayerController>();
+
+        missileTrigger = this.GetComponent<MissileTrigger>();
+        missileTrigger.enabled = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            missileTrigger.enabled = true;
+            missileTutorial.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        missileTutorial.SetActive(false);
+        missileTrigger.enabled = false;
+    }
+}
