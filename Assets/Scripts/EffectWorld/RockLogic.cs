e40cafcf8c514d1850fd11c66aab5d8031587eec
charles <Charles_cunningham@bloomfield.edu>
2019-04-06 13:47:46 -0400

    Rock Logic
    Big rocks can now be blown up

---
+++ Assets/Scripts/EffectWorld/RockLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockLogic : MonoBehaviour
+{
+    public GameObject miniRocks;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Missile"))
+        {
+            Instantiate(miniRocks, transform);
+            Vector3 jerk = new Vector3(5, 0, 0);
+            rb.AddForce(jerk, ForceMode.Impulse);
+            Destroy(other.gameObject);
+        }
+    }
+}
